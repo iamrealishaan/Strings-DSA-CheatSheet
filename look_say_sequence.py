210939dc1851c75bf64aa04b7fae372184de8cee
Ishaan <74845759+iamrealishaan@users.noreply.github.com>
2023-07-29 15:10:52 +0530

    Create look_say_sequence.py

---
+++ look_say_sequence.py
@@ -0,0 +1,14 @@
+#look and say sequence
+def next_num(s):
+    result = []
+    i = 0
+    while i<len(s):
+        count = 1
+        while i+1 < len(s) and s[i] == s[i+1]:
+            i+=1
+            count+=1
+        result.append(str(count)+s[i])
+        i+=1
+    return ''.join(result)
+s = "1211"
+print(next_num(s))
